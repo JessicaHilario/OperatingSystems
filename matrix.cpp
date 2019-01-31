75f7cba4dde8ae95ef213b554853da357fae89e1
Jessica <hilario.jessica97@gmail.com>
2019-01-31 11:51:15 -0800

    Assignment 1: Multithreading a matrix

---
+++ matrix.cpp
@@ -0,0 +1,15 @@
+//Jessica Hialrio
+//Joel Rodriguez
+//This program reads a matrix from a text file and outputs a 3x3 matrix
+
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+
+using namespace std;
+
+#define NUM_THREADS 9;
+
+int main() {
+	return 0;
+}
