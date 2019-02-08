7b5bd0123882410b929cfe6dd6913fcf0f9d3a15
Jessica Hilario <jessica.hilario@student.csulb.edu>
2019-02-08 11:08:25 -0800

    Final product of the assignment

--- Project1/matrix.cpp
+++ Project1/matrix.cpp
@@ -25,12 +25,37 @@ static const string TAG = "Main: ";
 int in1[3][3];
 int in2[3][3];
 int result[3][3];
+
+//A struct that holds the row and column
 struct info {
-	int i;
-	int j;
+	int row;
+	int col;
 };
 
+//Multiplies and adds the matrix by using the row and column from the struct
+void *matrix_mult(void *args) {
+	struct info *boi = (struct info*) args;
+	
+	//Multiplies and adds the row and column from the 2 matrices
+	for (int i = 0; i < MAX; i++) {
+		result[boi -> row][boi -> col] += in1[boi -> row][i] * in2[i][boi -> col];
+	}
+
+	//Return NULL
+	return NULL;
+}
+
+//Takes 2 input files of 3x3 matrices and produce a matrix by multiplying the threads
 int main(int argc, char *argv[]) {
+	int i, j;
+	
+	//Initialize the result matrix to 0
+	for (i = 0; i < MAX; i++) {
+		for (j = 0; j < MAX; j++) {
+			result[i][j] = 0;
+		}
+	}
+
 	string inFileName1 = "";
 	string inFileName2 = "";
 	ofstream outStream;
@@ -47,9 +72,17 @@ int main(int argc, char *argv[]) {
 	scan1.openFile(inFileName1);
 	scan2.openFile(inFileName2);
 
-	int i, j;
 
 	pthread_t threads[NUM_THREADS]; //Declares 9 threads
+
+	struct info doggo[MAX][MAX];
+
+	for (i = 0; i < MAX; i++) { //Create a 3D array of the struct info and store the indices
+		for (j = 0; j < MAX; j++) {
+			doggo[i][j].row = i;
+			doggo[i][j].col = j;
+		}
+	}
 	
 	cout << "Matrix 1" << endl;
 	for (i = 0; i < MAX; i++) { //Outputs first input file matrix
@@ -75,13 +108,15 @@ int main(int argc, char *argv[]) {
 		cout << endl;
 	}
 
-	for (i = 0; i < NUM_THREADS; i++) { //Creates the 9 threads
-		int *p;
-		cout << "creating thread: " << i << endl;
-		pthread_create(&threads[i], NULL, matrix_mult, (void*)(p))
+	for (i = 0; i < MAX; i++) { //Creates the 9 threads
+		for (j = 0; j < MAX; j++) {
+			cout << "creating thread: " << i << endl;
+			pthread_create(&threads[j + (MAX * i)], NULL, &matrix_mult, (void*) &doggo[i][j]);
+			//Index 1 dimensional array from a 2 dimensional array
+		}
 	}
 
-	for (i = 0; i < NUM_THREADS; i++) { //Joins and waits for all threads
+	for (i = 0; i < NUM_THREADS; i++) { //Joins all threads while the main thread waits
 		pthread_join(threads[i], NULL);
 	}
 	
